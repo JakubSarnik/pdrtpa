99ddc00279dc74709f963fd40df6ddfb2ec7e687
Jakub Šárník <jakub.sarnik@gmail.com>
2025-11-27 01:44:22 +0100

    Start writing the aiger builder.

--- src/aiger_builder.cpp
+++ src/aiger_builder.cpp
@@ -1,3 +1,46 @@
 #include "aiger_builder.hpp"
 
-// TODO
+namespace builder
+{
+
+namespace
+{
+
+bool is_true( const aiger_info& info, aiger_literal lit )
+{
+    return info.true_literals.contains( lit );
+}
+
+bool is_false( const aiger_info& info, aiger_literal lit )
+{
+    return info.true_literals.contains( aiger_not( lit ) );
+}
+
+bool is_decided( const aiger_info& info, aiger_literal lit )
+{
+    return is_true( info, lit ) || is_false( info, lit );
+}
+
+void propagate_trues( aiger_info& info )
+{
+    info.true_literals.emplace( aiger_true );
+
+    for ( auto i = 0u; i < info.aig->num_ands; ++i )
+    {
+        const auto [ lhs, rhs0, rhs1 ] = info.aig->ands[ i ];
+
+        if ( is_true( info, rhs0 ) && is_true( info, rhs1 ) )
+            info.true_literals.emplace( lhs );
+        else if ( is_false( info, rhs0 ) || is_false( info, rhs1 ) )
+            info.true_literals.emplace( aiger_not( lhs ) ); // NOLINT
+    }
+}
+
+aiger_literal get_error_literal( const aiger_info& info )
+{
+    return ( info.aig->num_outputs > 0 ? info.aig->outputs[ 0 ] : info.aig->bad[ 0 ] ).lit;
+}
+
+}
+
+}
--- src/aiger_builder.hpp
+++ src/aiger_builder.hpp
@@ -1,3 +1,59 @@
 #pragma once
 
-// TODO
+#include "logic.hpp"
+#include "transition_system.hpp"
+#include "aiger.h"
+#include <unordered_set>
+
+namespace builder
+{
+using aiger_literal = unsigned int;
+
+// Aiger files are first preprocessed to compute both the set of variables that
+// are constant throughout the computation (x is in true_literals if x is
+// constantly true, ~x is in true_literals is x is constantly false) and the
+// set of variables that can influence the error literal (error cone
+// of influence, those appear positively in error_coi). This information can
+// then be used to eliminate such variables from the formulae and decrease the
+// number of state variables.
+
+struct aiger_info
+{
+    aiger* aig;
+
+    // Aiger literals that are necessarily true. For any literal here, its
+    // negation aiger_not( lit ) is necessarily false. This is used to
+    // propagate constants and simplify the clausified formula. These don't
+    // need to be computed in the transition formula (i.e. can be set as
+    // x' = true or x' = false respectively), but they need to actually exist
+    // in the formula because they can occur in the error formula.
+    // Alternatively, they could be syntactically eliminated, or substituted
+    // for a pair of values xtrue and xfalse that are kept true and false,
+    // respectively, throughout the computation.
+    // TODO: If there is any true state variable, make the first one xtrue.
+    //       If there is any false state variable, make the first one xfalse.
+    std::unordered_set< aiger_literal > true_literals;
+
+    // The cone of influence of the error formula. Contains literals
+    // corresponding to the latches that are necessary to decide the value
+    // of the error literal. Any other latch x can have x' = false in the
+    // transition formula and this won't compromise correctness. Even better,
+    // any other latch can be deleted from the system completely (it does not
+    // appear in the error formula by definition).
+    std::unordered_set< aiger_literal > error_coi;
+
+    aiger_info( aiger_info& ) = delete;
+    aiger_info& operator=( const aiger_info& ) = delete;
+};
+
+struct context
+{
+    aiger_info preprocessed_aiger;
+
+    variable_range input_vars;
+    variable_range state_vars;
+    variable_range next_state_vars;
+    variable_range and_vars;
+};
+
+}
